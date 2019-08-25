300ba402fed852c09a88b154c9e59cba5d4e5d9f
James Thrasher <James.S.Thrasher@icloud.com>
2019-08-24 21:28:28 -0400

    added faker module and new scammer website

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
+import time
 import requests
 import os
 import random
 import string
 import json
+import faker
 
 chars = string.ascii_letters + string.digits + '!@#$%^&*()'
 random.seed = (os.urandom(1024))
 
-url = "http://127.0.0.1/login.php"  # Change to scammer's username and password POST API endpoint.
+url = "https://www.windowscarecancellation.com/refund/"  # Change to scammer's username and password POST API endpoint.
 
 names = json.loads(open("names.json").read())
 surnames = json.loads(open("surnames.json").read())
@@ -20,7 +22,7 @@ submitted = 1  # Keep track of total submitted/POSTs have occurred.
 
 while count <= 2:
     for name in names:
-        if submitted < 10000:  # Change maximum number of tries.
+        if submitted < 10000000:  # Change maximum number of tries.
             name_extra = ''.join(str(random.randint(0, 1000)))  # Adds a number after name between 0, 1000
 
             # Username variable is a name from list, a period, last name, random chars, and a random domain.
@@ -33,11 +35,28 @@ while count <= 2:
 
             # Commented out for testing purposes. Uncomment to POST.
             # requests.post(url, allow_redirects=False, data={
-            #     'j_username': username,  # `j_username` needs to be replaced with website's username form variable
-            #     'j_password': password  # `j_password` needs to be replaced with website's password form variable
+            #     'xuser': username,  # `j_username` needs to be replaced with website's username form variable
+            #     'xpass': password  # `j_password` needs to be replaced with website's password form variable
             # })
+
+            requests.post(url, allow_redirects=False, data={
+                "789_single_line_text_1": random.choice(names),  # first name
+                "789_single_line_text_2": random.choice(names),  # Middle name
+                "789_single_line_text_3": random.choice(surnames),  # Last name
+                "789_email_4": "",  # "email@email.com",
+                "happyforms-789_email_4_dummy_1566657981": "",  # "email@email.com",
+                "789_phone_5[number]": "",  # "5417537358",
+                "789_single_line_text_6": "",  # "address",
+                "789_single_line_text_7": "",  # "city",
+                "789_single_line_text_8": "",  # "state",
+                "789_single_line_text_9": "",  # "zipcode",
+                "789_single_line_text_10": "",  # "bankname",
+                "789_single_line_text_11": "",  # "599"
+            })
+
             print(f'{count}/{submitted}')
             print(f'{username} - {password}')
             # print(f'Sending username: {username} and password: {password}')
+            # time.sleep(2)
             submitted += 1
     count += 1
---
+++ main2.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+import time
+import requests
+import os
+import random
+import string
+import json
+from faker import Faker
+from faker.providers import phone_number, address
+
+test = False
+
+fake = Faker("en_US")
+fake.add_provider(phone_number)
+fake.add_provider(address)
+
+chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+random.seed = (os.urandom(1024))
+
+url = "https://www.windowscarecancellation.com/refund/"  # Change to scammer's username and password POST API endpoint.
+
+names = json.loads(open("names.json").read())
+surnames = json.loads(open("surnames.json").read())
+domains = json.loads(open("domains.json").read())
+words = json.loads(open("words.json").read())
+banks = json.loads(open("banks.json").read())
+
+count = 1  # Keep track of how many loops have occured through the list of names
+submitted = 2093  # Keep track of total submitted/POSTs have occurred.
+
+while submitted <= 500000:
+    email = random.choice(names).lower() + '.' + (random.choice(surnames).lower()) + '@' + random.choice(
+        domains).lower()
+
+    first_name = random.choice(names)  # first name
+    middle_name = random.choice(names)  # Middle name
+    last_name = random.choice(surnames).lower()  # Last name
+    email_address = first_name + '.' + last_name + '@' + random.choice(domains).lower()  # "email@email.com",
+    phone_number = fake.phone_number()  # "5417537358",
+    address = fake.street_address()  # "address",
+    city = fake.city()  # "city",
+    state = fake.state()  # "state",
+    zipcode = fake.zipcode()  # "zipcode",
+    bank = random.choice(banks)  # "bankname",
+    refund_amt = random.randint(100, 500)  # "599"
+
+    if not test:
+        data = {
+            "789_single_line_text_1": first_name,  # first name
+            "789_single_line_text_2": middle_name,  # Middle name
+            "789_single_line_text_3": last_name,  # Last name
+            "789_email_4": email,  # "email@email.com",
+            "happyforms-789_email_4_dummy_1566657981": email,  # "email@email.com",
+            "789_phone_5[number]": phone_number,  # "5417537358",
+            "789_single_line_text_6": address,  # "address",
+            "789_single_line_text_7": city,  # "city",
+            "789_single_line_text_8": state,  # "state",
+            "789_single_line_text_9": zipcode,  # "zipcode",
+            "789_single_line_text_10": bank,  # "bankname",
+            "789_single_line_text_11": refund_amt,  # "599"
+        }
+        requests.post(url, allow_redirects=False, data=data)
+        print(f'Sending data: {json.dumps(data, indent=4)}')
+
+    if test:
+        print(f'{first_name}')
+        print(f'{middle_name}')
+        print(f'{last_name}')
+        print(f'{email_address}')
+        print(f'{phone_number}')
+        print(f'{address}')
+        print(f'{city}')
+        print(f'{state}')
+        print(f'{zipcode}')
+        print(f'{bank}')
+        print(f'{refund_amt}')
+
+
+
+    submitted += 1
+    print(f"{submitted}\n\n\n")
+    time.sleep(.1)
+
